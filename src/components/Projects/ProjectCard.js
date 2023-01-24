9077d82bf2d38264a0eb8c1ec5fe130fc3b829dc
Adrie Buendia <adriebuendia@icloud.com>
2023-01-24 18:53:52 -0500

    Finished fixing and adding animations

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,9 +1,6 @@
 //imports
 import React from "react";
-import { FiGithub } from "react-icons/fi";
-
-//images
-import MusicalityImage from "../../assets/musicality-img.png";
+import { FiGithub, FiExternalLink } from "react-icons/fi";
 
 const ProjectCard = ({ data, cardIndex }) => {
   return (
@@ -12,13 +9,16 @@ const ProjectCard = ({ data, cardIndex }) => {
         <>
           <h2 className="project-name">{item.project}</h2>
           <div className="project-content">
-            <img src={item.image} alt="logo" />
+            <img src={item.image} alt="project" />
             <p className="project-description">{item.description}</p>
           </div>
           <div className="project-stack">
             <h3>{item.stack}</h3>
             <a href={item.github} target="_blank">
-              <FiGithub className="github-icon" />
+              <FiGithub className="link-icon" />
+            </a>
+            <a href={item.github} target="_blank">
+              <FiExternalLink className="link-icon" />
             </a>
           </div>
         </>
