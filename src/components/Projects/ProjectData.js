06718daf2e6780595d2a4a4c7bad5230dc25214b
Adrie Buendia <adriebuendia@icloud.com>
2023-01-25 00:44:04 -0500

    modified a few css styles

--- src/components/Projects/ProjectData.js
+++ src/components/Projects/ProjectData.js
@@ -2,7 +2,8 @@ export default [
   [
     {
       project: "Total Football",
-      stack: "ReactJS | HTML5 | CSS3 | NodeJS | Axios | Express | SQL",
+      stack:
+        "ReactJS | Javascript | HTML5 | CSS3 | NodeJS | Express | PostgreSQL",
       description:
         "Developed an app that allows the user to create a football team. Built RESTful API layer with Express/Node.js to modern specifications on top of a PostgreSQL DB. Designed a normalized relational database using SQL that stored user and football information, considered industry UI/UX standards when designing user interface, developed UI with React.js v18, allowing for scalable state/DOM management ",
       image: require("../../assets/total-football-img.png"),
@@ -12,7 +13,7 @@ export default [
   [
     {
       project: "Musicality",
-      stack: "Javascript | NodeJS | Express | HTML5 | CSS3 | Axios",
+      stack: "Javascript | NodeJS | Express | HTML5 | CSS3",
       description:
         "My first project ever, developed a simple app which allows users to add and delete songs in a playlist. Built Full-CRUD RESTful API with Express and Node.js. Developed modern reactive UI with ES6+ JS, HTML5, and CSS",
       image: require("../../assets/musicality-img.png"),
