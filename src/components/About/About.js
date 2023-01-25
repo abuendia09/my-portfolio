a0cdd08cd6a5f5a1f8cd2551dacc168acd0ea70f
Adrie Buendia <adriebuendia@icloud.com>
2023-01-25 00:10:25 -0500

    updated about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -30,25 +30,31 @@ const About = () => {
         <div className="about-content">
           <div className="paragraphs-container">
             <p className="about-paragraph">
-              Hi! My name is Adrie and I love coding and learning new things
-              every day. Even though I have an undergraduate degree in Economics
-              from the University of Louisville, I have always been driven
-              towards computers and programming.
+              Hi! My name is Adrie Buendia and welcome to my page. Born on July
+              9th, 1990 in Panama city, Panama. My family was military and we
+              were stationed in Panama. After the military based closed down, my
+              family loved it so much they decided to stay.
             </p>
             <p className="about-paragraph">
-              In 2022, I decided to make a career change and join
+              In 2013, I moved to Louisville, KY to pursue my undergraduate
+              degree in Economics at the University of Louisville.
+            </p>
+
+            <p className="about-paragraph">
+              I've always had love and passion for computers and programming. So
+              in 2022, I decided to make a career change. I joined
               <a href="https://devmountain.com/" target="_blank">
                 &nbsp;DevMountain&nbsp;
               </a>
-              and take part of their part time cohort focusing on Full Stack Web
-              Development, while I worked 60 hours a week.
+              and became a student in their part time cohort focusing on Full
+              Stack Web Development. All while I still worked 60 hours a week.
             </p>
             <p className="about-paragraph">
-              Now I'm focusing on improving my current skills and learning new
-              technologies as I search for a job that would allow me to break
-              into the tech world.
+              Today, I live in Florida and I'm focusing on improving my current
+              skills and learning new ones as I search for a job that would
+              allow me to break into the tech world.
             </p>
-            <p className="about-paragraph">Current technologies:</p>
+            <p className="about-paragraph">Current stack:</p>
             <ul className="stack-list">
               {stack && stack.map((stack, i) => <li key={i}>{stack}</li>)}
             </ul>
