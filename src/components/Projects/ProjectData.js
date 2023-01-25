238edd21a4861f069d034f3e855de2b00bb69321
Adrie Buendia <adriebuendia@icloud.com>
2023-01-24 19:37:40 -0500

    fixed the image positioning and added gradient background to projects and experience

--- src/components/Projects/ProjectData.js
+++ src/components/Projects/ProjectData.js
@@ -4,7 +4,7 @@ export default [
       project: "Total Football",
       stack: "ReactJS | HTML5 | CSS3 | NodeJS | Axios | Express | SQL",
       description:
-        "Developed a PERN stack app which allows user to create a football team based on the players that are on my Database. ",
+        "Developed an app that allows the user to create a football team. Built RESTful API layer with Express/Node.js to modern specifications on top of a PostgreSQL DB. Designed a normalized relational database using SQL that stored user and football information, considered industry UI/UX standards when designing user interface, developed UI with React.js v18, allowing for scalable state/DOM management ",
       image: require("../../assets/total-football-img.png"),
       github: "https://github.com/abuendia09/total-football-capstone",
     },
@@ -14,7 +14,7 @@ export default [
       project: "Musicality",
       stack: "Javascript | NodeJS | Express | HTML5 | CSS3 | Axios",
       description:
-        "My first ever project, developed a simple app which allows users to add songs to a default playlist that is provided.",
+        "My first project ever, developed a simple app which allows users to add and delete songs in a playlist. Built Full-CRUD RESTful API with Express and Node.js. Developed modern reactive UI with ES6+ JS, HTML5, and CSS",
       image: require("../../assets/musicality-img.png"),
       github: "https://www.github.com/abuendia09/pt4-capstone",
     },
