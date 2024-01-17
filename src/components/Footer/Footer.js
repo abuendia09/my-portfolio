87422008f9395533f53cb62dc186660fd00533df
Adrie Buendia <adriebuendia@icloud.com>
2024-01-17 09:48:23 -0500

    Fixed the social media links in the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,13 +9,25 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <div className="social-icons">
-        <a href="https://www.github.com/abuendia09" target="_blank">
+        <a
+          href="https://www.github.com/abuendia09"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FiGithub className="github-icon" />
         </a>
-        <a href="https://www.linkedin.com/in/adriebuendia" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/adriebuendia"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FiLinkedin className="linkedin-icon" />
         </a>
-        <a href="https://www.instagram.com/adriebuendia" target="_blank">
+        <a
+          href="https://www.instagram.com/adriebuendia"
+          target="_blank"
+          rel="noreferrer"
+        >
           <FiInstagram className="instagram-icon" />
         </a>
       </div>
